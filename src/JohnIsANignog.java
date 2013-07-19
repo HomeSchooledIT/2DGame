bccf4d8f1d7a104ea8dc78cedce45753c03190ac
] <homeschooledit@gmail.com>
2013-07-18 19:28:16 -0500

    john is definitely a nignog

---
+++ src/JohnIsANignog.java
@@ -0,0 +1,16 @@
+
+public class JohnIsANignog {
+	boolean isNogging = true;
+	int yes = 0;
+	public static void Main(String Args[]){
+		
+	}
+	public void isJohnANignog(){
+		while(yes != 100){
+		
+		System.out.println("John is a nignog!");
+		yes++;
+		
+		}
+	}
+}
