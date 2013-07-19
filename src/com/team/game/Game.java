27c9cf2e78d457ef7471ffa6d9d34c37edc62890
John <John Masterson@192.168.1.105>
2013-07-18 21:40:13 -0500

    its a start

--- src/com/team/game/Game.java
+++ src/com/team/game/Game.java
@@ -1,5 +1,35 @@
 package com.team.game;
 
-public class Game {
+import java.awt.Canvas;
 
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+	public static int width = 300;
+	public static int height = width * 9 / 16;
+	public static int scale = 3;
+	
+	private Thread thread;
+	private JFrame frame;
+	
+	public synchronized void start(){
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+	
+	public synchronized void stop(){
+		try{
+			thread.join();
+		} catch (InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	public void run(){
+		
+	}
+	
 }
